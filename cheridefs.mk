0fbe4f754a8acc1cab744b96ae044416405a88ad
Munraj Vadera <mbv21@cam.ac.uk>
2014-10-09 14:47:25 +0100

    [gzip_u, libz_u, toplevel] Add support for building for CHERI.

---
+++ cheridefs.mk
@@ -0,0 +1,6 @@
+CHERI_SDK?=/scratch/cheri-tools/sdk
+CHERI_SYSROOT?=/scratch/cheri-tools/Build_old/sdk
+CC:=PATH=$(CHERI_SDK)/bin:$(CHERI_SYSROOT)/bin $(CHERI_SDK)/bin/clang
+CFLAGS+=--sysroot=$(CHERI_SYSROOT)
+CPOSTFLAGS+=-target cheri-unknown-freebsd -msoft-float -B$(CHERI_SYSROOT)
+
