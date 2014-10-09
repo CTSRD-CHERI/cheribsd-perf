2175744e1d13aa4558d59a6a934815084dda3b48
Munraj Vadera <mbv21@cam.ac.uk>
2014-10-09 15:17:10 +0100

    [gzip_u] Add push target to Makefile.CHERI.

--- cheridefs.mk
+++ cheridefs.mk
@@ -3,4 +3,4 @@ CHERI_SYSROOT?=/scratch/cheri-tools/Build_old/sdk
 CC:=PATH=$(CHERI_SDK)/bin:$(CHERI_SYSROOT)/bin $(CHERI_SDK)/bin/clang
 CFLAGS+=--sysroot=$(CHERI_SYSROOT)
 CPOSTFLAGS+=-target cheri-unknown-freebsd -msoft-float -B$(CHERI_SYSROOT)
-
+CHERI_SSH_HOST=oregano
