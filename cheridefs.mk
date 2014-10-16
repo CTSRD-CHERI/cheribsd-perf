3f4805682e67a4909e505a83ef191e4a261dfb0d
Munraj Vadera <mbv21@cam.ac.uk>
2014-10-16 11:46:19 +0100

    Add $NM to cheridefs.mk so that libz_u build works OK.

--- cheridefs.mk
+++ cheridefs.mk
@@ -3,6 +3,7 @@ CHERI_SDK_BIN:=$(CHERI_SDK)/tools/bin
 CHERI_SANDBOX_LD:=$(CHERI_SDK)/usr/libdata/ldscripts/sandbox.ld
 
 CC:=PATH=$(CHERI_SDK_BIN):$(CHERI_SDK)/bin $(CHERI_SDK_BIN)/clang
+NM:=$(CHERI_SDK)/bin/nm
 GIT_COMMIT_STRING!=git log | grep commit | head -n 1 | cut -d" " -f2 | sed s/^/\\\\\"/ | sed s/$$/\\\\\"/
 CFLAGS+=--sysroot=$(CHERI_SDK) -DGIT_COMMIT_STRING=$(GIT_COMMIT_STRING)
 CPOSTFLAGS+=-target cheri-unknown-freebsd -msoft-float -B$(CHERI_SDK)
