adaf321c87aa5f6a7f41e29fc10a7db194cd0d9b
Munraj Vadera <mbv21@cam.ac.uk>
2014-10-31 13:53:07 +0000

    [gzip_all] Update Makefile to use individual push targets and fix test.sh.

--- gzip_all/test.sh
+++ gzip_all/test.sh
@@ -8,12 +8,12 @@ dotest ()
 
 dotestn ()
 {
-  i=0
-  while [ $i -ne $2 ] ; do dotest $1 $3 ; i=`expr $i + 1` ; done
+  j=0
+  while [ $j -ne $2 ] ; do dotest $1 $3 ; j=`expr $j + 1` ; done
 }
 
-mult=10000
-nrun=2
+mult=100000
+nrun=10
 ntestrun=1
 
 generate ()
@@ -26,24 +26,33 @@ do
   sz=`expr $i \* $mult`
   echo dd if=ZERO of=ZERO$i bs=$sz count=1
   dd if=ZERO of=ZERO$i bs=$sz count=1
+  echo dd if=RANDOM of=RANDOM$i bs=$sz count=1
   dd if=RANDOM of=RANDOM$i bs=$sz count=1
+  echo dd if=ENTROPY of=ENTROPY$i bs=$sz count=1
   dd if=ENTROPY of=ENTROPY$i bs=$sz count=1
   i=`expr $i + 1`
 done
 }
 
+runtests ()
+{
 i=1
 while [ $i -ne $nrun ]
 do
-  #sz=`expr $i \* $mult`
+  sz=`expr $i \* $mult`
   echo "---------BEGIN RUN $i ($sz bytes) --------"
-  #echo -e "ZERO$i\nRANDOM$i\nENTROPY$i\n" > file_list
-  echo -e "ZERO\nRANDOM\nENTROPY\n" > file_list
+  echo -e "ZERO$i\nRANDOM$i\nENTROPY$i\n" > file_list
+  #echo -e "ZERO\nRANDOM\nENTROPY\n" > file_list
   cat file_list
-  dotestn ./gzip_u $ntestrun "(unmodified gzip+zlib)"
+  dotestn ./gzip_u $ntestrun "(unmodified gzip + unmodified zlib)"
+  dotestn ./gzip_u_libz_c $ntestrun "(unmodified gzip + capability-only zlib)"
   dotestn ./gzip_a $ntestrun "(Capsicum gzip)"
-  dotestn ./gzip_u_libz_h $ntestrun "(libcheri zlib)"
+  dotestn ./gzip_u_libz_a $ntestrun "(unmodified gzip + Capsicum zlib)"
+  dotestn ./gzip_u_libz_h $ntestrun "(unmodified gzip + libcheri zlib)"
   dotestn ./gzip_h $ntestrun "(libcheri gzip)"
   echo "---------END RUN $i ($sz bytes) --------"
   i=`expr $i + 1`
 done
+}
+
+$@
